5ac2a728404a7c8e45973c89e27779f8ece74903
pearlgreen <james.wall@pearlgreen.co.uk>
2014-10-27 19:19:04 +0000

    missed commits

---
+++ src/main/java/RawData.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+/**
+ * Created by James on 27/10/2014.
+ */
+public class RawData extends ArrayList {
+
+
+
+    public String parseSymbol() {
+
+        return "TODO";
+
+    }
+
+
+
+public String parseName() {
+
+    return "TODO";
+
+    }
+
+    public String parseSector() {
+
+        return "TODO";
+    }
+
+    public double parseMarketCap() {
+
+        return 0.00;
+
+    }
+
+}
--- src/test/java/TestBuildData.java
+++ src/test/java/TestBuildData.java
@@ -30,7 +30,7 @@ public class TestBuildData {
     }
 
 
-    
+
 
     @Test
     public void testListIsFilteredCorrectly(){
